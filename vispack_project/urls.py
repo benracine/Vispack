fd9fa75dbe3e8ff3dd05f9cb3740f49dce214b87
Ben Racine <i3enhamin@gmail.com>
2011-10-06 20:37:15 -0700

    not sure

--- vispack_project/urls.py
+++ vispack_project/urls.py
@@ -1,17 +1,77 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from django.views.generic.list_detail import object_list
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vispack_project.views.home', name='home'),
-    # url(r'^vispack_project/', include('vispack_project.foo.urls')),
+from homepage.views import homepage
+from package.views import package_autocomplete, category, packaginate
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns("",
+
+    url('', include('social_auth.urls')),
+    url(r"^$", homepage, name="home"),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^profiles/", include("profiles.urls")),
+    url(r"^packages/", include("package.urls")),
+    url(r"^grids/", include("grid.urls")),
+    url(r"^search/", include("searchv1.urls")),
+    url(r"^feeds/", include("feeds.urls")),
+
+    url(r"^categories/(?P<slug>[-\w]+)/$", category, name="category"),
+    url(r"^categories/$", homepage, name="categories"),
+    url(r"^packaginator/$",
+                direct_to_template,
+                {'template': 'package/packaginator.html'},
+                name="packaginator"),
+
+    url(r"^packaginate/$",
+                packaginate,
+                name="packaginate"),
+
+    url(
+        regex = '^autocomplete/package/$',
+        view = package_autocomplete,
+        name    = 'package_autocomplete',
+    ),
+    url(r'^login/$', direct_to_template, {'template': 'pages/login.html', }, 'login',),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {}, 'logout',),
+
+    # static pages
+    url(r"^about/$", direct_to_template, {"template": "pages/about.html"}, name="about"),
+    url(r"^terms/$", direct_to_template, {"template": "pages/terms.html"}, name="terms"),
+    url(r"^faq/$", direct_to_template, {"template": "pages/faq.html"}, name="faq"),    
+    url(r"^syndication/$", direct_to_template, {"template": "pages/syndication.html"}, name="syndication"),
+    url(r"^contribute/$", direct_to_template, {"template": "pages/contribute.html"}, name="contribute"),
+
 )
+
+from apiv1.api import Api
+from apiv1.resources import (
+                    GotwResource, DpotwResource,
+                    PackageResource, CategoryResource,
+                    GridResource, PackageResourceBase,
+                    UserResource
+                    )
+
+v1_api = Api()
+v1_api.register(PackageResourceBase())
+v1_api.register(PackageResource())
+v1_api.register(CategoryResource())
+v1_api.register(GridResource())
+v1_api.register(GotwResource())
+v1_api.register(DpotwResource())
+v1_api.register(UserResource())
+
+urlpatterns += patterns('',
+    url(r"^api/", include(v1_api.urls)),
+)
+
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns("",
+        url(r"", include("django.contrib.staticfiles.urls")),
+    )
